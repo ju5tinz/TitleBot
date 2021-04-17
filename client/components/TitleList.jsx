2ed1a80115acd11fc89dcc308db3164f148fb46b
Justin Zhang <ju5tinz.mlearn@gmail.com>
2021-04-17 16:01:06 -0700

    open links in new tab

--- client/components/TitleList.jsx
+++ client/components/TitleList.jsx
@@ -6,13 +6,13 @@ export default TitleList = ({ list }) => {
       {list.length > 0 &&
         <div>
           <div className="d-flex justify-content-center list-main-item">
-            <div><a href={list[0].url}>{list[0].title}</a></div>
+            <div><a target="_blank" href={list[0].url}>{list[0].title}</a></div>
           </div>
 
           <div>
             {list.slice(1).map(e =>
               <div className="d-flex justify-content-center list-sec-item">
-                <div><a href={e.url}>{e.title}</a></div>
+                <div><a target="_blank" href={e.url}>{e.title}</a></div>
               </div>
             )}
           </div>
