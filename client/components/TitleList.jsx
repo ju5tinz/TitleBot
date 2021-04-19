87cf766477711df227f17be6f776e1ad57833329
Justin Zhang <ju5tinz.mlearn@gmail.com>
2021-04-19 13:12:10 -0700

    fix key warning

--- client/components/TitleList.jsx
+++ client/components/TitleList.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 export default TitleList = ({ list }) => {
+  console.log(list)
+
   return(
     <>
       {list.length > 0 &&
@@ -10,8 +12,8 @@ export default TitleList = ({ list }) => {
           </div>
 
           <div>
-            {list.slice(1).map(e =>
-              <div className="d-flex justify-content-center list-sec-item">
+            {list.slice(1).map((e, index) =>
+              <div key={index} className="d-flex justify-content-center list-sec-item">
                 <div><a target="_blank" href={e.url}>{e.title}</a></div>
               </div>
             )}
